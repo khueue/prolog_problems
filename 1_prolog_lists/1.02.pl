1c36ddca1df2671fa014104d8b875898bd7d4970
khueue <khueue@gmail.com>
2011-01-11 18:55:50 +0100

    Added 1.02.

---
+++ 1_prolog_lists/1.02.pl
@@ -0,0 +1,23 @@
+%   Prolog Problems - Prolog Lists
+%   http://sites.google.com/site/prologsite/prolog-problems/1
+
+:- module(p1_02, [last_but_one/2]).
+
+:- include('../common').
+
+%   last_but_one(?LastButOneElement, +List)
+%
+%   True if LastButOneElement is the last but one element of List.
+
+test(last_but_one/2,
+    [ true
+    , fail:last_but_one(_, [])
+    , fail:last_but_one(_, [a])
+    , last_but_one(a, [a,b])
+    , last_but_one(b, [a,b,c])
+    , one:last_but_one(_, [a,b,c])
+    ]).
+
+last_but_one(X, [X,_]) :- !.
+last_but_one(X, [_|Xs]) :-
+    last_but_one(X, Xs).
