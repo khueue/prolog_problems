08eb6b59fc53c1930f41502fed64e13f5132a293
khueue <khueue@gmail.com>
2011-01-14 15:33:24 +0100

    Renamed files to match modules.

---
+++ 1_prolog_lists/p1_05.pl
@@ -0,0 +1,28 @@
+%   Prolog Problems - Prolog Lists
+%   http://sites.google.com/site/prologsite/prolog-problems/1
+
+:- module(p1_05, [my_reverse/2]).
+
+:- include('../common').
+
+%%  my_reverse(+List, ?Reversed)
+%
+%   True if Reversed is List in reverse order.
+
+test(my_reverse/2,
+    [ true
+    , my_reverse([], [])
+    , my_reverse([a], [a])
+    , my_reverse([a,b], [b,a])
+    , my_reverse([a,b,c], [c,b,a])
+    , my_reverse([a,b,c], R1), R1 == [c,b,a]
+    , one:my_reverse([a,b,c], _)
+    ]).
+
+my_reverse(List, Reversed) :-
+    my_reverse(List, [], Reversed).
+
+% Tail-recursive helper.
+my_reverse([], Rev, Rev).
+my_reverse([X|Xs], Rev0, Rev) :-
+    my_reverse(Xs, [X|Rev0], Rev).
---
+++ 1_prolog_lists/p1_06.pl
@@ -0,0 +1,25 @@
+%   Prolog Problems - Prolog Lists
+%   http://sites.google.com/site/prologsite/prolog-problems/1
+
+:- module(p1_06, [is_palindrome/1]).
+
+:- use_module(p1_05, [my_reverse/2]).
+
+:- include('../common').
+
+%%  is_palindrome(+List)
+%
+%   True if List is a palindrome.
+
+test(is_palindrome/1,
+    [ true
+    , is_palindrome([])
+    , is_palindrome([a])
+    , fail:is_palindrome([a,b])
+    , is_palindrome([a,b,a])
+    , is_palindrome([a,b,b,a])
+    , is_palindrome([a,b,c,b,a])
+    ]).
+
+is_palindrome(List) :-
+    my_reverse(List, List).
